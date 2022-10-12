44a433d10ca5ff9341bd29f718091b83cf15326b
denniswiersma <dennis.wiersma@icloud.com>
2022-10-12 15:17:12 +0200

    added main.py template

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+Module description
+"""
+
+# METADATA #
+
+# IMPORTS #
+import sys
+
+
+# CODE #
+
+def main(args):
+    """Function description"""
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
