c1b417282fdfd4b227eb6ef35abd9b0e6b95999b
denniswiersma <dennis.wiersma@icloud.com>
2022-11-06 21:26:24 +0100

    removed classes for the sake of classes

--- main.py
+++ main.py
@@ -19,101 +19,95 @@ from Bio.Seq import MutableSeq
 
 
 # CODE #
-class MSAHandler:
 
-    def __init__(self, path_to_protein_family):
-        self.path_to_protein_family = path_to_protein_family
+def msa_from_fasta(path_to_protein_family):
+    """
+    Use Clustalo on the command line to open a fasta file and align it.
 
-    def msa_from_fasta(self):
-        """
-        Use Clustalo on the command line to open a fasta file and align it.
-
-        :return: MSA data in fasta format
-        """
-        try:
-            msa_file = os.popen("/Users/denniswiersma/clustalo -i " + self.path_to_protein_family)
-            return msa_file
-        except ChildProcessError:
-            print("Clustalo does not function correctly. Please check your install.")
-
-    def read_msa(self):
-        """
-        Read the MSA file using BioPython's AlignIO object.
-
-        :return: AlignIO object containing the MSA data.
-        """
-        try:
-            return AlignIO.read(self.msa_from_fasta(), "fasta")
-        except FileNotFoundError:
-            print("Could not read MSA.")
+    :return: MSA data in fasta format
+    """
+    try:
+        msa_file = os.popen("/Users/denniswiersma/clustalo -i " + path_to_protein_family)
+        return msa_file
+    except ChildProcessError:
+        print("Clustalo does not function correctly. Please check your install.")
 
 
-class SNPHandler:
-    def __init__(self, dna_seq_or_file):
-        self.dna_seq_or_file = dna_seq_or_file
+def read_msa(msa_data):
+    """
+    Read the MSA file using BioPython's AlignIO object.
 
-    def read_dna(self):
-        """
-        Takes either a DNA sequence directly or a fasta file and extracts it.
+    :return: AlignIO object containing the MSA data.
+    """
+    try:
+        return AlignIO.read(msa_data, "fasta")
+    except FileNotFoundError:
+        print("Could not read MSA.")
 
-        :return: A SecRecord object.
-        """
 
-        # Check if sequence or filename was entered
-        if (i.upper() in "ATCG" for i in self.dna_seq_or_file):
+def read_dna(dna_seq_or_file):
+    """
+    Takes either a DNA sequence directly or a fasta file and extracts it.
 
-            # Prompt user for said parameters
-            print("Looks like you've entered a DNA sequence. "
-                  "You can optionally provide some more data. You may leave these empty if you wish.")
-            seq_id = input("Please enter a sequence id: ")
-            seq_name = input("Please enter a sequence name: ")
-            seq_desc = input("Please enter a sequence description: ")
+    :return: A SecRecord object.
+    """
 
-            # Return a SeqRecord instance
-            return SeqRecord(
-                MutableSeq(self.dna_seq_or_file),
-                id=seq_id,
-                name=seq_name,
-                description=seq_desc
-            )
+    # Check if sequence or filename was entered
+    if (i.upper() in "ATCG" for i in dna_seq_or_file):
 
-        else:
-            try:
-                # Open fasta file containing DNA sequence
-                with open(self.dna_seq_or_file) as dna_file:
-                    return SeqIO.parse(dna_file, "fasta")
-            except FileNotFoundError:
-                print("Could not read DNA sequence file.")
+        # Prompt user for said parameters
+        print("Looks like you've entered a DNA sequence. "
+              "You can optionally provide some more data. You may leave these empty if you wish.")
+        seq_id = input("Please enter a sequence id: ")
+        seq_name = input("Please enter a sequence name: ")
+        seq_desc = input("Please enter a sequence description: ")
 
-    def insert_snp(self, protein_dna_record, snp_position, snp_letter):
-        """
-        Inserts an SNP into a DNA sequence extracted from a SeqRecord with MutableSeq.
-
-        :param protein_dna_record: SeqRecord object containing the DNA to insert the SNP into.
-        :param snp_position: Position at which the SNP should be placed.
-        :param snp_letter: Letter that should replace the letter already present.
-        :return:
-        """
-        protein_dna = protein_dna_record.seq
-
-        if snp_position == 0:
-            protein_dna = snp_letter + protein_dna
-
-        elif snp_position <= len(protein_dna):
-            protein_dna[snp_position - 1] = snp_letter
-
-        elif snp_position > len(protein_dna):
-            protein_dna += snp_letter
-
-        else:
-            raise IndexError("Could not resolve SNP position.")
-
+        # Return a SeqRecord instance
         return SeqRecord(
-            MutableSeq(protein_dna),
-            id=protein_dna_record.id,
-            name=protein_dna_record.name,
-            description=protein_dna_record.description
+            MutableSeq(dna_seq_or_file),
+            id=seq_id,
+            name=seq_name,
+            description=seq_desc
         )
+
+    else:
+        try:
+            # Open fasta file containing DNA sequence
+            with open(dna_seq_or_file) as dna_file:
+                return SeqIO.parse(dna_file, "fasta")
+        except FileNotFoundError:
+            print("Could not read DNA sequence file.")
+
+
+def insert_snp(protein_dna_record, snp_position, snp_letter):
+    """
+    Inserts an SNP into a DNA sequence extracted from a SeqRecord with MutableSeq.
+
+    :param protein_dna_record: SeqRecord object containing the DNA to insert the SNP into.
+    :param snp_position: Position at which the SNP should be placed.
+    :param snp_letter: Letter that should replace the letter already present.
+    :return:
+    """
+    protein_dna = protein_dna_record.seq
+
+    if snp_position == 0:
+        protein_dna = snp_letter + protein_dna
+
+    elif snp_position <= len(protein_dna):
+        protein_dna[snp_position - 1] = snp_letter
+
+    elif snp_position > len(protein_dna):
+        protein_dna += snp_letter
+
+    else:
+        raise IndexError("Could not resolve SNP position.")
+
+    return SeqRecord(
+        MutableSeq(protein_dna),
+        id=protein_dna_record.id,
+        name=protein_dna_record.name,
+        description=protein_dna_record.description
+    )
 
 
 def get_arguments():
@@ -145,15 +139,13 @@ def main(args):
     """Function description"""
     arguments = get_arguments()
 
-    msa_handler = MSAHandler(arguments.ProteinFile)
-    aligned_msa = msa_handler.read_msa()
+    aligned_msa = read_msa(arguments.ProteinFile)
 
-    snp_handler = SNPHandler(arguments.sequence)
-    protein_dna = snp_handler.read_dna()
+    protein_dna = read_dna(arguments.sequence)
 
     snp_position = int(arguments.snp[0])
     snp_letter = arguments.snp[1]
-    protein_dna_snp = snp_handler.insert_snp(protein_dna, snp_position, snp_letter)
+    protein_dna_snp = insert_snp(protein_dna, snp_position, snp_letter)
 
     return 0
 
